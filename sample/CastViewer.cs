239d806cef0e5da1422a57afb19083dd3578d2f5
shinp <shinpeiyoshihara@gmail.com>
2018-09-24 15:45:01 +0900

    Caster implements

---
+++ sample/CastViewer.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SaitamaTec.CastTime
+{
+	[RequireComponent(typeof(UnityEngine.UI.Button))]
+	[RequireComponent(typeof(UnityEngine.UI.Image))]
+	[RequireComponent(typeof(Collider))]
+  public class CastViewer : MonoBehaviour
+  {
+
+		[SerializeField]
+    public CastTimer WatchTarget;
+
+		public UnityEngine.UI.Text TimeLabel;
+		public UnityEngine.UI.Button CastButton;
+
+		public void OnMouseDown(){
+			Debug.Log("NNNN");
+		}
+
+		public void Start(){
+			this.CastButton.onClick.AddListener(
+				()=>{
+					Debug.Log("ボタン押したで！");
+					WatchTarget.Cast();
+				}
+			);
+		}
+		public void Update(){
+			TimeLabel.text=TimeLabelString;
+
+			CastButton.interactable = WatchTarget.Castable;
+		}
+
+		public string TimeLabelString{
+			get{
+				return WatchTarget.Castable
+					?"Rady!"
+					:WatchTarget.RecastingTime.ToString("0.00");
+			}			
+		}
+  }
+
+}
---
+++ src/CastTimer.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace SaitamaTec.CastTime
+{
+  public class CastTimer : MonoBehaviour
+  {
+		[SerializeField]
+		private float recastTime=10;
+		[SerializeField]
+		private float castTime=10;
+
+		
+		private float castingTime=0;
+		private float recastingTime=0;
+
+
+		public float RecastTime{
+			get{
+				return recastTime;
+			}
+		}
+		
+
+		public bool Castable{
+			get{
+				return RecastingTime <= 0;
+			}
+		}		
+		public float RecastingTime{
+			get{
+				return recastingTime;
+			}
+		}
+
+		public int RecastTimeFrame{
+			set{
+
+			}
+		}
+
+		public void Cast(){
+			recastingTime = RecastTime;
+		}
+
+		public void Start(){
+			recastingTime = RecastTime;
+		}
+
+		
+		public void Update(){
+			recastingTime-=Time.deltaTime;
+
+		}
+  }
+
+}
